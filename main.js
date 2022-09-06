da17ff4c5a1c48f4759cbcbd4aaed139dd9722d1
squareclouds <sd@squareclouds.de>
2022-09-06 20:09:21 +0200

    gltf with picker now working, after that gltf draco and gets saved to desktop, must be tested on windows

--- main.js
+++ main.js
@@ -1,24 +1,61 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, ipcMain, BrowserWindow } = require('electron')
+//const path = require('path')
+const savepath = require('path').join(require('os').homedir(), 'Desktop')
 require('electron-reload')(__dirname);
+
+/* 3d compression */
+const gltfPipeline = require("gltf-pipeline");
+const fsExtra = require("fs-extra");
+const obj2gltf = require("obj2gltf");
+const fs = require("fs");
 
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
-        fullscreen: true,
+        /* fullscreen: true,
         alwaysOnTop: true,
         autoHideMenuBar: true,
-        kiosk :true,
+        titleBarStyle: 'hidden', 
+        kiosk :true,*/
         center: true,
-        titleBarStyle: 'hidden',
+
         webPreferences: {
             //preload: path.join(__dirname, 'preload.js')
+            nodeIntegration: true,
+            contextIsolation: false,
         }
     });
 
+    console.log(savepath);
     win.loadFile('www/index.html');
+}
+
+function convertToGltf(args,page) {
+    obj2gltf(args['file']).then(function (gltf) {
+        const data = Buffer.from(JSON.stringify(gltf));
+        var filenamegltf = args['filename'];
+        fs.writeFileSync(savepath+"/"+filenamegltf+"-regular.gltf", data);
+        page.send('mainprocess-response', "GLTF done");
+
+        compressWithDraco(filenamegltf,page);
+    });  
+}
+
+function compressWithDraco(filename,page) {
+
+    const processGltf = gltfPipeline.processGltf;
+    const gltf = fsExtra.readJsonSync(savepath+"/"+filename+"-regular.gltf");
+    const options = {
+        dracoOptions: {
+            compressionLevel: 10,
+        },
+    };
+    processGltf(gltf, options).then(function (results) {
+        fsExtra.writeJsonSync(savepath+"/"+filename+"-draco_compressed.gltf", results.gltf);
+        page.send('mainprocess-response', "GLTF Draco done");
+    });
 }
 
 app.whenReady().then(() => {
@@ -28,3 +65,11 @@ app.whenReady().then(() => {
 app.on('window-all-closed', function () {
     app.quit();
 })
+
+// Attach listener in the main process with the given ID
+ipcMain.on('request-mainprocess-action', (event, arg) => {
+    console.log(
+        arg
+    );
+    convertToGltf(arg, event.sender);    
+});
--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -1,123 +1,42 @@
-let serverURL;
-let STATION_ID;
+const { ipcRenderer } = require("electron");
+//const mainProcess = app.require('./main.js');
 
-const COUNTER_MAX = 45;
-let counter = COUNTER_MAX;
-let interval;
 let timeoutfeedback;
 
-let countdown_el;
+var btn_convert;
 
 
 function initialize() {
+  btn_convert = document.getElementById('convert');
+  btn_convert.addEventListener('click', () => {
+    let file_field = document.getElementById("objfile");
+    if (file_field.files.length == 0) {
+      setFeedback("No files selected");
+      return;
+    }
 
-  handleStationID();
+    console.log("yes");
 
-  serverURL = window.localStorage.serverURL;
-  document.getElementById('server').innerHTML = serverURL;
+    let Data = {
+      file: file_field.files[0].path,
+      filename: removeExtension(file_field.files[0].name)
+    };
 
-  if (!serverURL) serverURL = '';
+    ipcRenderer.send('request-mainprocess-action', Data);
 
-  //document.getElementById('input__server').value = serverURL;
-
-  countdown_el = document.getElementById('countdown');
-  countdown_el.innerHTML = counter;
-
-  document.getElementById('load').addEventListener('click', async () => {
-    load();
-  })
-
-  document.getElementById('save').addEventListener('click', async () => {
-    saveServer();
-  })
-
-  document.getElementById('stopTimer').addEventListener('click', async () => {
-    stopTimer();
-  })
-
-  document.getElementById('saveID').addEventListener('click', async () => {
-    saveID();
-  })
-
-  document.getElementById('generateID').addEventListener('click', async () => {
-    generateID();
-  })
-
-  document.getElementById('input__server').addEventListener('input', function (ev) {
-    checkInput(ev.target);
-  });
-  document.getElementById('input__id').addEventListener('input', function (ev) {
-    checkInput(ev.target);
+    // Add the event listener for the response from the main process
+    ipcRenderer.on('mainprocess-response', (event, arg) => {
+      console.log(arg); // prints "Hello World!"
+      setFeedback(arg);
+    });
   });
 
-  checkInput(document.getElementById('input__server'));
-  checkInput(document.getElementById('input__id'));
-
-  interval = setInterval(function () {
-    tick()
-  }, 1000);
 }
 
-function stopTimer() {
-  clearInterval(interval);
-  countdown_el.innerHTML = "-";
+function removeExtension(filename) {
+  return filename.substring(0, filename.lastIndexOf('.')) || filename;
 }
 
-function handleStationID() {
-  STATION_ID = window.localStorage.STATION_ID;
-  if (!STATION_ID) {
-    generateID();
-  }
-  document.getElementById('header__station_id').innerHTML = STATION_ID;
-}
-
-function generateID() {
-  var newID = STATION_ID = (+new Date).toString(36);
-  //STATION_ID = (+new Date).toString(36);
-  //window.localStorage.setItem('STATION_ID', STATION_ID);
-  document.getElementById('input__id').value = newID;
-  document.getElementById('saveID').disabled = false;
-}
-
-function saveID() {
-  //serverURL = document.getElementById('input__server').value;
-  STATION_ID = document.getElementById('input__id').value;
-  //window.localStorage.setItem('serverURL', serverURL);
-  window.localStorage.setItem('STATION_ID', STATION_ID);
-  document.getElementById('input__id').value = "";
-  document.getElementById('header__station_id').innerHTML = STATION_ID;
-
-  setFeedback("Station ID saved!");
-}
-
-function saveServer() {
-  serverURL = document.getElementById('input__server').value;
-  window.localStorage.setItem('serverURL', serverURL);
-  document.getElementById('server').innerHTML = serverURL;
-  document.getElementById('input__server').value = "";
-  setFeedback("Server URL saved!");
-}
-
-function checkInput(input) {
-  var target_btn = document.getElementById(input.getAttribute('data-target'));
-  var val = input.value;
-  if (val && String(val).length >= 3) {
-    target_btn.disabled = false;
-  }
-  else {
-    target_btn.disabled = true;
-  }
-}
-
-function tick() {
-  countdown_el.innerHTML = counter;
-
-  if (counter == 0) {
-    load();
-  }
-
-  counter--;
-}
 
 function setFeedback(message) {
   clearTimeout(timeoutfeedback);
@@ -126,64 +45,6 @@ function setFeedback(message) {
 }
 function resetFeedback() {
   document.getElementById('feedback').innerHTML = "";
-}
-
-function load() {
-  let url = serverURL.replace(/\/+$/, '') + "/" + STATION_ID;
-
-  clearInterval(interval);
-
-  if (isValidURL(url)) {
-    try {
-      preloadURL(url);
-    }
-    catch (e) {
-      console.log('wrong');
-    }
-  }
-  else {
-    setFeedback("URL was not valid!");
-    counter = COUNTER_MAX;
-    interval = setInterval(function () {
-      tick()
-    }, 1000);
-  }
-
-}
-
-
-function isValidURL(url) {
-  var res = url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-  return (res !== null)
-}
-
-function preloadURL(url) {
-  fetch(url)
-    .then(manageErrors) // call function to handle errors
-    .then(function (response) {
-
-      window.location.replace(url);
-    }).catch(function (error) {
-      setFeedback("Your ID is not registered on the given server, or the server is offline!");
-      counter = COUNTER_MAX;
-      interval = setInterval(function () {
-        tick()
-      }, 1000);
-
-      console.log('Error Code   : ' + error.status);
-      console.log('Error Reason : ' + error.statusText);
-    });
-}
-
-function manageErrors(response) {
-  if (!response.ok) {
-    const responseError = {
-      statusText: response.statusText,
-      status: response.status
-    };
-    throw (responseError);
-  }
-  return response;
 }
 
 function quit() {
