ec3f82f00a1e18ce1510f36a52e96872eb661bc6
squareclouds <sd@squareclouds.de>
2022-09-07 12:33:50 +0200

    preview images now working

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, ipcMain, BrowserWindow } = require('electron')
+const FileSaver = require('file-saver');
 //const path = require('path')
 const savepath = require('path').join(require('os').homedir(), 'Desktop')
 require('electron-reload')(__dirname);
@@ -12,12 +13,13 @@ const fs = require("fs");
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 900,
+        //titleBarStyle: 'hidden', 
+        autoHideMenuBar: true,
         /* fullscreen: true,
         alwaysOnTop: true,
-        autoHideMenuBar: true,
-        titleBarStyle: 'hidden', 
+                
         kiosk :true,*/
         center: true,
 
@@ -28,33 +30,63 @@ function createWindow() {
         }
     });
 
-    console.log(savepath);
     win.loadFile('www/index.html');
 }
 
-function convertToGltf(args,page) {
+
+
+function convertToGltf(args, page) {
     obj2gltf(args['file']).then(function (gltf) {
         const data = Buffer.from(JSON.stringify(gltf));
         var filenamegltf = args['filename'];
-        fs.writeFileSync(savepath+"/"+filenamegltf+"-regular.gltf", data);
-        page.send('mainprocess-response', "GLTF done");
+        fs.writeFileSync(savepath + "/" + filenamegltf + "-regular.gltf", data);
+        page.send('mainprocess-response', "GLTF created");
+
+        let Data = {
+            label: "Regular GLTF",
+            name: filenamegltf,
+            path: savepath + "/" + filenamegltf + "-regular.gltf",
+            closer: false
+        };
+
+        page.send('mainprocess-objpath', Data);
 
         compressWithDraco(filenamegltf,page);
-    });  
+    });
 }
 
-function compressWithDraco(filename,page) {
+function savePreviewImg(args, page){
+    console.log("saving image");
+    var filename = args['filename'];
+    //fs.writeFileSync(savepath + "/" + filename, args['file']);
 
+    FileSaver.saveAs(args['file'],savepath + "/" + filename);
+   // fs.createWriteStream(savepath + "/" + filename).write(args['file']);
+
+    page.send('mainprocess-response', "Preview image created"); 
+}
+
+function compressWithDraco(filename, page) {
     const processGltf = gltfPipeline.processGltf;
     const gltf = fsExtra.readJsonSync(savepath+"/"+filename+"-regular.gltf");
+    //const gltf = fsExtra.readJsonSync(args['file']);
     const options = {
         dracoOptions: {
-            compressionLevel: 10,
+            compressionLevel: 1,
         },
     };
     processGltf(gltf, options).then(function (results) {
-        fsExtra.writeJsonSync(savepath+"/"+filename+"-draco_compressed.gltf", results.gltf);
-        page.send('mainprocess-response', "GLTF Draco done");
+        fsExtra.writeJsonSync(savepath + "/" + filename + "-draco_compressed.gltf", results.gltf);
+        page.send('mainprocess-response', "GLTF Draco created");
+
+        let Data = {
+            label: "Draco compressed GLTF",
+            name: filename,
+            path: savepath + "/" + filename + "-draco_compressed.gltf",
+            closer: true
+        };
+
+        page.send('mainprocess-objpath', Data);
     });
 }
 
@@ -67,9 +99,37 @@ app.on('window-all-closed', function () {
 })
 
 // Attach listener in the main process with the given ID
-ipcMain.on('request-mainprocess-action', (event, arg) => {
+ipcMain.on('request-gltf', (event, arg) => {
     console.log(
         arg
     );
-    convertToGltf(arg, event.sender);    
+    convertToGltf(arg, event.sender);
+});
+
+/* // Attach listener in the main process with the given ID
+ipcMain.on('request-previewimg', (event, arg) => {
+    console.log(
+        arg
+    );
+    savePreviewImg(arg, event.sender);
+}); */
+
+// Attach listener in the main process with the given ID
+ipcMain.on('request-gltf-draco', (event, arg) => {
+    console.log(
+        arg
+    );
+    compressWithDraco(arg, event.sender);
+});
+
+
+
+ipcMain.on('request-previewimg', (event, path, buffer) => {
+    fsExtra.outputFile(savepath+"/"+path, buffer, err => {
+        if (err) {
+            event.sender.send('ERROR_FILE', err.message)
+        } else {
+            event.sender.send('request-previewimg-saved', savepath+"/"+path)
+        }
+    })
 });
--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -1,37 +1,132 @@
+
 const { ipcRenderer } = require("electron");
 //const mainProcess = app.require('./main.js');
 
 let timeoutfeedback;
 
 var btn_convert;
+//var btn_compress;
+var btn_reset;
 
+let regularGltfPath;
+let filename;
 
 function initialize() {
   btn_convert = document.getElementById('convert');
+  //btn_compress = document.getElementById('compress');
+  btn_reset = document.getElementById('reset');
+
   btn_convert.addEventListener('click', () => {
     let file_field = document.getElementById("objfile");
+    btn_convert.classList.add('disabled');
     if (file_field.files.length == 0) {
       setFeedback("No files selected");
       return;
     }
 
-    console.log("yes");
+    filename = removeExtension(file_field.files[0].name);
 
     let Data = {
       file: file_field.files[0].path,
-      filename: removeExtension(file_field.files[0].name)
+      filename: filename
     };
 
-    ipcRenderer.send('request-mainprocess-action', Data);
-
-    // Add the event listener for the response from the main process
-    ipcRenderer.on('mainprocess-response', (event, arg) => {
-      console.log(arg); // prints "Hello World!"
-      setFeedback(arg);
-    });
+    ipcRenderer.send('request-gltf', Data);
   });
 
+  /* btn_compress.addEventListener('click', () => {
+
+    let Data = {
+      file: regularGltfPath,
+      filename: filename
+    };
+    ipcRenderer.send('request-gltf-draco', Data);
+  }); */
+
+  btn_reset.addEventListener('click', () => {
+
+    document.getElementById('viewers').innerHTML = "";
+
+    let form = document.getElementById('form-obj');
+    form.reset();
+  });
+
+  ipcRenderer.on('mainprocess-response', (event, arg) => {
+    setFeedback(arg);
+  });
+
+  ipcRenderer.on('mainprocess-objpath', (event, args) => {
+    //btn_compress.classList.remove('hidden');
+    btn_reset.classList.remove('hidden');
+
+    let model_id = Date.now() + Math.random();
+
+    let viewerDiv = document.createElement("div");
+    viewerDiv.classList.add('model');
+    viewerDiv.innerHTML = '<h2>' + args['label'] + '</h2><p>' + args['name'] + '</p><model-viewer id="' + model_id + '" class="model-viewer" src="' + args['path'] + '" id="reveal" loading="eager" camera-controls touch-action="pan-y" auto-rotate shadow-intensity="1" alt="loaded model"></model-viewer>';
+
+    document.getElementById('viewers').appendChild(viewerDiv);
+    regularGltfPath = args['path'];
+
+    if (args['closer']) {
+      viewerDiv.classList.add('closer');
+      btn_convert.classList.remove('disabled');
+    }
+    else {
+      const modelViewer = document.getElementById(model_id);
+      //const img = document.getElementById('img_' + model_id);
+
+      modelViewer.addEventListener('load', function () {
+        let data = modelViewer.toBlob({
+          idealAspect: true
+        }).then(function (imgdata) {
+          console.log("succeesss image");
+          console.log(imgdata);
+
+          //let _imgdata = URL.createObjectURL(imgdata)
+          //img.src = _imgdata;
+
+          /* const file = new File([imgdata], "export.png");
+          const link = document.createElement("a");
+          link.download = file.name;
+          link.href = URL.createObjectURL(file);
+          link.click(); */
+
+          /* let imginfo = {
+            file: imgdata,
+            filename: args['name'] + '-preview.png'
+          }; */
+
+          //ipcRenderer.send('request-previewimg', imginfo);
+          saveBlob(imgdata, args['name'] + '-preview.png');
+        });
+      });
+    }
+
+
+  });
 }
+
+
+function saveBlob(blob, fileName) {
+  let reader = new FileReader()
+  reader.onload = function () {
+    if (reader.readyState == 2) {
+      var buffer = Buffer.from(reader.result)
+      ipcRenderer.send('request-previewimg', fileName, buffer)
+      console.log(`Saving ${JSON.stringify({ fileName, size: blob.size })}`)
+    }
+  }
+  reader.readAsArrayBuffer(blob)
+}
+
+ipcRenderer.on('SAVED_FILE', (event, path) => {
+  console.log('request-previewimg-saved ' + path)
+});
+/* 
+ipcRenderer.on('ERROR_FILE', (event, msg) => {
+  console.log("Error " + msg)
+}); */
 
 function removeExtension(filename) {
   return filename.substring(0, filename.lastIndexOf('.')) || filename;
@@ -42,9 +137,11 @@ function setFeedback(message) {
   clearTimeout(timeoutfeedback);
   timeoutfeedback = setTimeout(resetFeedback, 2000);
   document.getElementById('feedback').innerHTML = message;
+  document.getElementById('feedback').classList.add('show');
 }
 function resetFeedback() {
-  document.getElementById('feedback').innerHTML = "";
+  //document.getElementById('feedback').innerHTML = "";
+  document.getElementById('feedback').classList.remove('show');
 }
 
 function quit() {
